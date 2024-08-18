11d31da5e6650b20862657b9e673a77b5213c055
Sherlock Holmes <sherlockdetective69@gmail.com>
2024-08-18 18:31:34 +0530

    Update About.js
    Updated LinkedIn code

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
     const content = `I'm Chetan Akash, Final Year Computer Science Undergrad at Bits Pilani, Hyderabad Campus. My interests lie in the fields of Machine Learning, Competitive Coding, and Web Development. 
 
 I’m a highly motivated individual and passionate about my work. I have a steady source of motivation that drives me to do my best. I have good Problem-Solving Skills. I look Forward to Using my Skills and Knowledge to Contribute as much as possible to the Computer Science field.`;
-    const linkedIn = "https://www.linkedin.com/in/chetanakash-vankadara-3a0548217";
+    const linkedIn = "https://www.linkedin.com/in/chetan-akash-vankadara-3a0548217/";
     return(
         <div className="auth-container" >
          <Navbar isSelected = "About"/> 
