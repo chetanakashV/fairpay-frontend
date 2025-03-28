be12838bdc611b3c3187a411f72e94945bb7a6ca
Sherlock Holmes <sherlockdetective69@gmail.com>
2025-03-29 00:09:53 +0530

    Dashboard revamp (#9)
    * modified test page
    
    * dashboard-revamp

---
+++ src/Pages/Test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '2rem',
+            color: '#1cc29f',
+            fontWeight: 'bold',
+            textAlign: 'center'
+        }}>
+            🚀 The application is currently up and running! 🚀 
+        </div>
+    )
+}
+
+export default Test;
